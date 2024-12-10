402c39fa6917ae4d8aaa5ca36b81c7a0ec405f5e
bobi-usa <bobi_usa@163.com>
2024-12-10 17:47:52 +0800

    完成antD-mobile主题色的定制化(1. 全局定制 2. 局部定制)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import sum from '@/test'
 import router from './router';
 import { RouterProvider } from 'react-router-dom';
 
+// 导入定制主题文件
+import './theme.css'
+
 console.log(sum(6, 6))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom"
+import { Button } from 'antd-mobile'
 
 const Layout = () => {
   return (
     <div>
       {/* 如果不显示二级路由内容，要么是没有写出口，要么是出口放的位置不对，应该放在对应的一级路由中 */}
       <Outlet />
+
       我是Layout
+      {/* 测试全局生效样式 */}
+      <Button color='primary'>测试全局</Button>
+      <div className="purple">
+        <Button color='primary'>测试局部</Button>
+      </div>
     </div>
   )
 }
