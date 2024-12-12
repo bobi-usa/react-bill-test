3befbdab081ee3726e0df26d9128973f4dd24846
bobi-usa <bobi_usa@163.com>
2024-12-12 10:58:54 +0800

    月度账单：切换弹框显示关闭、箭头向上向下

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,16 +1,21 @@
 import { NavBar, DatePicker } from 'antd-mobile'
 import './index.scss'
+import { useState } from 'react'
+import classNames from 'classnames'
 
 const Month = () => {
+  // 控制弹框的打开和关闭
+  const [dateVisible, setDateVisible] = useState(false)
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backIcon={false}>月度收支</NavBar>
       <div className="content">
         <div className="header">
           {/* 时间切换区域 */}
-          <div className="date">
+          <div className="date" onClick={() => { setDateVisible(true) }}>
             <span className="text">2023 | 3月账单</span>
-            <span className="arrow expand"></span>
+            {/* 思路：根据当前弹框打开的状态控制expand类名是否存在 */}
+            <span className={classNames("arrow", dateVisible && 'expand')}></span>
           </div>
           {/* 统计区域 */}
           <div className="twoLineOverview">
@@ -32,8 +37,9 @@ const Month = () => {
             className="KaDate"
             title='记账日期'
             precision="month"
-            visible={false}
+            visible={dateVisible}
             max={new Date()}
+            onClose={() => { setDateVisible(false) }}
           />
         </div>
       </div>
