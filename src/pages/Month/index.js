c3fb11de77726ed340ae8f67f45e722f66edf6ef
bobi-usa <bobi_usa@163.com>
2024-12-12 10:23:10 +0800

    月度账单：统计区域面板html和css

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,5 +1,44 @@
+import { NavBar, DatePicker } from 'antd-mobile'
+import './index.scss'
+
 const Month = () => {
-  return <div>我是Month</div>
+  return (
+    <div className="monthlyBill">
+      <NavBar className="nav" backIcon={false}>月度收支</NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div className="date">
+            <span className="text">2023 | 3月账单</span>
+            <span className="arrow expand"></span>
+          </div>
+          {/* 统计区域 */}
+          <div className="twoLineOverview">
+            <div className="item">
+              <div className="money">{100}</div>
+              <div className="type">支出</div>
+            </div>
+            <div className="item">
+              <div className="money">{200}</div>
+              <div className="type">收入</div>
+            </div>
+            <div className="item">
+              <div className="money">{200}</div>
+              <div className="type">结余</div>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className="KaDate"
+            title='记账日期'
+            precision="month"
+            visible={false}
+            max={new Date()}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Month
